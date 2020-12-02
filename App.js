66ae10bb34ec77ed379666ecbf24e4d34042dfdd
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-12-01 22:15:13 -0800

    create app

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi this is the barebones for</Text>
+      <Text>Nathan and Sam's scheduling app</Text>
       <StatusBar style="auto" />
     </View>
   );
