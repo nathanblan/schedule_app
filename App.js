62584df413f1f6f7b5adad94462882e291e4a258
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-12-02 16:31:39 -0800

    background image

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground, Button, Text, View, Image } from 'react-native';
 
-const image = { "./home.png" };
+//retrieve image for background
+const background = { uri: "https://raw.githubusercontent.com/nathanblan/schedule_app/master/assets/home.png" };
 
 export default function App() {
+
+  /*This does not yet work, and I have no idea why
+  <View style={styles.container}>
+    <ImageBackground source={background} style={styles.image}></ImageBackground>
+  </View>
+  */
+
+  //define the default and changed text as objects in an array, call the next index onPress
   const [outputText, setOutputText] = useState("This is Nathan and Sam's scheduling app");
+
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText("The text changed")}/>
+      <ImageBackground source={background} style={styles.image}>
+
+        <View style={styles.text}>
+          <Text>{outputText}</Text>
+          <Button title="Change Text" onPress={() => setOutputText("The text changed")}/>
+        </View>
+         
+      </ImageBackground>
     </View>
   );
 }
@@ -17,13 +33,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column"
   },
   image: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    alignItems: "center"
   }
 });
