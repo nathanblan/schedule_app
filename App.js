4fbeed840dc7d4a91e428e2e830b6a432eca267b
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-12-07 20:58:07 -0800

    Add daily agendas

--- App.js
+++ App.js
@@ -27,13 +27,12 @@ export default function App() {
         <View>
           <CalendarList
             style={styles.calendar}
-            
+
             // Collection of dates that have to be colored in a special way. Default = {}
             markedDates={{
-              '2012-05-20': { textColor: 'green' },
-              '2012-05-22': { startingDay: true, color: 'green' },
-              '2012-05-23': { selected: true, endingDay: true, color: 'green', textColor: 'gray' },
-              '2012-05-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
+              '2020-05-22': { startingDay: true, color: 'green' },
+              '2020-05-23': { endingDay: true, color: 'green' },
+              '2020-05-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
             }}
             // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
             markingType={'period'}
@@ -74,6 +73,67 @@ export default function App() {
             showScrollIndicator={true}
 
             onDayPress={(day) => { console.log('selected day', day) }}
+          />
+        </View>
+
+        <View>
+          <Agenda
+            // The list of items that have to be displayed in agenda. If you want to render item as empty date
+            // the value of date key has to be an empty array []. If there exists no value for date key it is
+            // considered that the date in question is not yet loaded
+            items={{
+              '2012-05-22': [{ name: 'item 1 - any js object' }],
+              '2012-05-23': [{ name: 'item 2 - any js object', height: 80 }],
+              '2012-05-24': [],
+              '2012-05-25': [{ name: 'item 3 - any js object' }, { name: 'any js object' }]
+            }}
+            // Callback that gets called when items for a certain month should be loaded (month became visible)
+            loadItemsForMonth={(month) => { console.log('trigger items loading') }}
+            // Callback that fires when the calendar is opened or closed
+            onCalendarToggled={(calendarOpened) => { console.log(calendarOpened) }}
+            // Callback that gets called on day press
+            onDayPress={(day) => { console.log('day pressed') }}
+            // Callback that gets called when day changes while scrolling agenda list
+            onDayChange={(day) => { console.log('day changed') }}
+            // Initially selected day
+            selected={'2012-05-16'}
+            // Specify how each item should be rendered in agenda
+            renderItem={(item, firstItemInDay) => { return (<View />); }}
+            // Specify how each date should be rendered. day can be undefined if the item is not first in that day.
+            renderDay={(day, item) => { return (<View />); }}
+            // Specify how empty date content with no items should be rendered
+            renderEmptyDate={() => { return (<View />); }}
+            // Specify how agenda knob should look like
+            renderKnob={() => { return (<View />); }}
+            // Specify what should be rendered instead of ActivityIndicator
+            renderEmptyData={() => { return (<View />); }}
+            // Specify your item comparison function for increased performance
+            rowHasChanged={(r1, r2) => { return r1.text !== r2.text }}
+            // Hide knob button. Default = false
+            hideKnob={true}
+            // By default, agenda dates are marked if they have at least one item, but you can override this if needed
+            markedDates={{
+              '2012-05-16': { selected: true, marked: true },
+              '2012-05-17': { marked: true },
+              '2012-05-18': { disabled: true }
+            }}
+            // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
+            disabledByDefault={true}
+            // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
+            onRefresh={() => console.log('refreshing...')}
+            // Set this true while waiting for new data from a refresh
+            refreshing={false}
+            // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
+            refreshControl={null}
+            // Agenda theme
+            theme={{
+              agendaDayTextColor: 'yellow',
+              agendaDayNumColor: 'green',
+              agendaTodayColor: 'red',
+              agendaKnobColor: 'blue'
+            }}
+            // Agenda container style
+            style={{}}
           />
         </View>
 
