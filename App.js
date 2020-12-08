1420917c431f8ef5449f771f119d3d20c0d7b7ed
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-12-07 20:38:12 -0800

    Event markers
    trying to add event markers

--- App.js
+++ App.js
@@ -27,6 +27,17 @@ export default function App() {
         <View>
           <CalendarList
             style={styles.calendar}
+            
+            // Collection of dates that have to be colored in a special way. Default = {}
+            markedDates={{
+              '2012-05-20': { textColor: 'green' },
+              '2012-05-22': { startingDay: true, color: 'green' },
+              '2012-05-23': { selected: true, endingDay: true, color: 'green', textColor: 'gray' },
+              '2012-05-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
+            }}
+            // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
+            markingType={'period'}
+
             theme={{
               backgroundColor: '#f00bde',
               calendarBackground: 'rgba(255,255,255,0)',
